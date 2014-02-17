888d7137c9e8aab6bbeecec48737bb33d43329e4
kenrett <kenrettberg@gmail.com>
2014-02-17 13:10:33 -0800

    passing test for code length

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,4 +1,4 @@
 class Program < ActiveRecord::Base
-  validates_presence_of :title, :subtitle
-  
+  validates_presence_of :title, :subtitle, :code
+  validates_length_of :code, :maximum => 6, too_long: "is too long (maximum is 6 characters)"
 end
--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -3,5 +3,7 @@ require 'spec_helper'
 describe Program do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:subtitle) }
+  it { should validate_presence_of(:code) }
+  it { should ensure_length_of(:code).is_at_most(6) }
   
 end
