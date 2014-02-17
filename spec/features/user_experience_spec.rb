c171b2a6da4634594813e3fdea79f8761bc3166b
kenrett <kenrettberg@gmail.com>
2014-02-17 15:50:51 -0800

    added failing test for creating new program

--- spec/features/user_experience_spec.rb
+++ spec/features/user_experience_spec.rb
@@ -8,16 +8,16 @@ describe 'a user visiting the homepage', js: true do
     expect(page).to have_content('TCI')
   end
 
-  it 'should be able to see links for more information' do
+  it 'can see links for more information' do
     visit root_path
     click_link('TCI')
     expect(page).to have_content('rocks')
   end
 
-  # it 'should be able to click on a button to create a new program' do
-  #   visit root_path
-  #   click_button('Create a new program')
-  #   expect(page).to have_content()
-  # end 
+  it 'can create a new program' do
+    visit root_path
+    click_button('Create a new program')
+    expect(page).to have_content('Create New Program')
+  end 
 end
   
